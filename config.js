9c7ea8f0e50fa878a5c185e5b3658070449ced02
pquanhihi <101121906+pquanhihi@users.noreply.github.com>
2022-03-07 21:55:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 const CONFIG = {
     titleWeb: "U là trời web gì lạ zậy",
-    introTitle: 'Tên người yêu bạn',
+    introTitle: 'Hi bé Pae',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Có yêu mình hong trả lời 🥰',
+    desc: 'Chọn thật lòng nha tui nhìn thấy hết đó ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Lỡ nghiện rồi không dứt ra được đâu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Bae của tui 8/3 vui vẻ nha. Được nhiều người chúc nè, được ba mẹ, em gái và cả ny chụt chụt nè. Buồn ghê 8/3 phải ở nhà rồi không làm gì được cho bae hết. Đừng buồn nha. Hôm sau tớ sẽ bù đắp lại nhé. Iu em',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
