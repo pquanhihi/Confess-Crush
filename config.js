d4b47211f4615dd6df20bfd85bd346fdba95107c
pquanhihi <101121906+pquanhihi@users.noreply.github.com>
2022-03-07 22:41:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,14 +6,14 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Có yêu mình hong trả lời 🥰',
+    title: 'Có yêu mình hong trả lời đi pé iu dấu 🥰',
     desc: 'Chọn thật lòng nha tui nhìn thấy hết đó ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Lỡ nghiện rồi không dứt ra được đâu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    mess: 'Mình biết mà 🥰. Yêu bé nhiều nhiều 😘😘',
     messDesc: 'Bae của tui 8/3 vui vẻ nha. Được nhiều người chúc nè, được ba mẹ, em gái và cả ny chụt chụt nè. Buồn ghê 8/3 phải ở nhà rồi không làm gì được cho bae hết. Đừng buồn nha. Hôm sau tớ sẽ bù đắp lại nhé. Iu em',
     btnAccept: 'Okiiiii lun <3',
 }
