9f70bda8f4d281a08848f4e37a659b6f5f72a146
pquanhihi <101121906+pquanhihi@users.noreply.github.com>
2022-03-07 21:45:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
+    titleWeb: "U là trời web gì lạ zậy",
     introTitle: 'Tên người yêu bạn',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
